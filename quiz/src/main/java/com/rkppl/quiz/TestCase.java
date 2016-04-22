f0213ae4999573cbf81f212f15f31dfe75de25f5
hendar <hendar.133040294@mail.unpas.ac.id>
2016-04-22 14:01:50 +0700

    first commint

--- quiz/src/main/java/com/rkppl/quiz/TestCase.java
+++ quiz/src/main/java/com/rkppl/quiz/TestCase.java
@@ -11,6 +11,6 @@ package com.rkppl.quiz;
  */
 public class TestCase {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello A");
     }
 }
